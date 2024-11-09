1fb66bc0b8eecabe7e27c632f71016206402b988
Amishi Mishra <mishmish@unc.edu>
2024-11-09 15:14:36 -0500

    s

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function Start(){
-    const question = document.createElement("p");
+    const question = document.createElement("div");
+    question.setAttribute("class", "question")
+    const statement = document.createElement("h2")
     const node = document.createTextNode("This is my question?");
-    question.appendChild(node);
+    statement.appendChild(node);
+    question.appendChild(statement)
 
-    const element = document.getElementByClass("bigBox");
-    element.appendChild(question)
+    const element = document.getElementById("bigBox");
+    const remove = document.getElementById("delete")
+    element.replaceChild(question, remove)
 }
