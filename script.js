a25685a3c005769c51856a1be2e3dcefbfa5bd17
Amishi Mishra <mishmish@unc.edu>
2024-11-09 14:50:46 -0500

    s

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function Start{
-    
+function Start(){
+    const question = document.createElement("p");
+    const node = document.createTextNode("This is my question?");
+    question.appendChild(node);
+
+    const element = document.getElementByClass("bigBox");
+    element.appendChild(question)
 }
