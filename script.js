7e4e8aeb8ce04c6350fc09ada7c4099c52917367
Amishi Mishra <mishmish@unc.edu>
2024-11-09 16:56:58 -0500

    major list

--- script.js
+++ script.js
@@ -1,6 +1,83 @@
-function Start(){
-    
+class major{
+    constructor(name, points, link) {
+        this.name = name
+        this.points = 0
+        this.link = link
+    }
 }
+
+function makeMajorList(){
+    const africanStudies = new major('African, African American, and Diaspora Studies Major', 'https://catalog.unc.edu/undergraduate/programs-study/african-african-american-diaspora-studies-major-ba/')
+    const americanStudies = new major('American Studies Major','https://catalog.unc.edu/undergraduate/programs-study/american-studies-major-ba/' )
+    const anthropology = new major('Anthropology Major', 'https://catalog.unc.edu/undergraduate/programs-study/anthropology-major-ba/')
+    const archaeology = new major('Archaeology Major','https://catalog.unc.edu/undergraduate/programs-study/archaeology-major-ba/' ) 
+    const artHistory = new major('Art History Major','https://catalog.unc.edu/undergraduate/programs-study/art-history-major-ba/' )
+    const asianStudies = new major('Asian Studies Major','https://catalog.unc.edu/undergraduate/programs-study/asian-studies-major-ba-general-concentration/' )
+    const biology = new major('Biology Major','https://catalog.unc.edu/undergraduate/programs-study/biology-major-bs/' )
+    const bioMedEngineering = new major('Biomedical Engineering Major','https://catalog.unc.edu/undergraduate/programs-study/biomedical-engineering-major-bs/' )
+    const biostats = new major ('Biostatistics Major','https://catalog.unc.edu/undergraduate/programs-study/biostatistics-major-bsph/' )
+    const business = new major('Business Administration Major','https://catalog.unc.edu/undergraduate/programs-study/business-administration-major-bsba/' )
+    const chemistry = new major('Chemistry Major', 'https://catalog.unc.edu/undergraduate/programs-study/chemistry-major-bs/')
+    const classics = new major('Classics Major','https://catalog.unc.edu/undergraduate/programs-study/classics-major-ba-classical-archaeology/' )
+    const clinicalLabSci = new major('Clinical Laboratory Science Major','https://catalog.unc.edu/undergraduate/programs-study/clinical-laboratory-science-major-bs/' )
+    const communication = new major('Communication Studies Major','https://catalog.unc.edu/undergraduate/programs-study/communication-studies-major-ba/' )
+    const compSci = new major ('Computer Science Major','https://catalog.unc.edu/undergraduate/programs-study/computer-science-major-bs/' )
+    const contempEuroStudies = new major('Contemporary European Studies Major','https://catalog.unc.edu/undergraduate/programs-study/contemporary-european-studies-major-ba/' )
+    const dataSci = new major('Data Science Major','https://catalog.unc.edu/undergraduate/programs-study/data-science-major-bs/' )
+    const dentalHygeine = new major('Dental Hygiene Major','https://catalog.unc.edu/undergraduate/programs-study/dental-hygiene-major-bs/' )
+    const drama = new major('Dramatic Art Major','https://catalog.unc.edu/undergraduate/programs-study/dramatic-art-major-ba/' )
+    const earthMarineSci = new major('Earth and Marine Sciences Major','https://catalog.unc.edu/undergraduate/programs-study/earth-marine-sciences-major-bs/' )
+    const econ = new major('Economics Major', 'https://catalog.unc.edu/undergraduate/programs-study/economics-major-bs/')
+    const english = new major('English and Comparative Literature Major','https://catalog.unc.edu/undergraduate/programs-study/english-comparative-major-ba/' )
+    const enviHealthSci = new major('Environmental Health Sciences Major', 'Environmental Health Sciences Major, B.S.P.H.')
+    const enviSci = new major('Environmental Science Major','https://catalog.unc.edu/undergraduate/programs-study/environmental-science-bs/' )
+    const exerciseSportSci = new major('Exercise and Sport Science Major','https://catalog.unc.edu/undergraduate/programs-study/exercise-sport-science-major-ba-general/' )
+    const geography = new major('Geography Major', 'https://catalog.unc.edu/undergraduate/programs-study/geography-major-ba/')
+    const geologicSci = new major('Geological Sciences Major', 'https://catalog.unc.edu/undergraduate/programs-study/geological-sciences-major-ba-earth-science-concentration/')
+    const germSlavLang = new major('Germanic and Slavic Languages and Literatures Major', 'https://catalog.unc.edu/undergraduate/programs-study/germanic-slavic-languages-literatures-major-slavic-east-european-languages-cultures-concentration/')
+    const globalStudies = new major('Global Studies Major', 'https://catalog.unc.edu/undergraduate/programs-study/global-studies-major-ba/')
+    const healthPolicyMngmt = new major('Health Policy and Management Major','https://catalog.unc.edu/undergraduate/programs-study/health-policy-management-major-bsph/' )
+    const history = new major('History Major', 'https://catalog.unc.edu/undergraduate/programs-study/history-major-ba/')
+    const humOrganizLeadDev = new major('Human and Organizational Leadership Development Major','https://catalog.unc.edu/undergraduate/programs-study/human-org-leadership-ba/' )
+    const humDevFamSci = new major('Human Development and Family Science Major', 'https://catalog.unc.edu/undergraduate/programs-study/human-development-family-studies-baed/')
+    const infoSci = new major('Information Science Major', 'https://catalog.unc.edu/undergraduate/programs-study/information-science-major-bs/')
+    const interdisciplinaryStudies = new major('Interdisciplinary Studies Major', 'https://catalog.unc.edu/undergraduate/programs-study/interdisciplinary-studies-major-ba/')
+    const latinAmericanStudies = new major('Latin American Studies Major','https://catalog.unc.edu/undergraduate/programs-study/latin-american-studies-major-ba/' )
+    const linguistics = new major('Linguistics Major','https://catalog.unc.edu/undergraduate/programs-study/linguistics-major-ba/' )
+    const womenGenderStudies = new major('Women’s and Gender Studies Major', 'https://catalog.unc.edu/undergraduate/programs-study/womens-gender-studies-major-ba/')
+    const studioArt = new major('Studio Art Major','https://catalog.unc.edu/undergraduate/programs-study/studio-art-major-ba/' )
+    const stats = new major('Statistics and Analytics Major','https://catalog.unc.edu/undergraduate/programs-study/statistics-analytics-majors-bs/' )
+    const sociology = new major('Sociology Major','https://catalog.unc.edu/undergraduate/programs-study/sociology-major-ba/' )
+    const romanceLang  = new major('Romance Languages Major','https://catalog.unc.edu/undergraduate/programs-study/romance-languages-major-ba-french-francophone-studies/' )
+    const religiousStudies = new major('Religious Studies Major','https://catalog.unc.edu/undergraduate/programs-study/religious-studies-major-ba/' )
+    const radioSci = new major('Radiologic Science Major', 'https://catalog.unc.edu/undergraduate/programs-study/radiologic-science-major-bs/')
+    const publicPolicy = new major('Public Policy Major','https://catalog.unc.edu/undergraduate/programs-study/public-policy-major-ba/' )
+    const psych = new major('Psychology Major','https://catalog.unc.edu/undergraduate/programs-study/psychology-major-bs/' )
+    const poliSci = new major('Political Science Major','https://catalog.unc.edu/undergraduate/programs-study/political-science-major-ba/' )
+    const phsyics = new major('Physics Major','https://catalog.unc.edu/undergraduate/programs-study/physics-major-bs/' )
+    const philosophy = new major('Philosophy Major','https://catalog.unc.edu/undergraduate/programs-study/philosophy-major-ba/' )
+    const peaceWarDefense = new major('Peace, War, and Defense Major','https://catalog.unc.edu/undergraduate/programs-study/peace-war-defense-major-ba/' )
+    const nutrition = new major('Nutrition Major', 'https://catalog.unc.edu/undergraduate/programs-study/nutrition-major-bsph/')
+    const nursing = new major('Nursing Major','https://catalog.unc.edu/undergraduate/programs-study/nursing-major-bsn/' )
+    const neuro = new major('Neuroscience Major', 'https://catalog.unc.edu/undergraduate/programs-study/neuroscience-major-bs/')
+    const neurodiagnosticSleepSci = new major('Neurodiagnostics and Sleep Science Major','https://catalog.unc.edu/undergraduate/programs-study/neurodiagnostics-sleep-sciences-major-bs/' )
+    const music = new major('Music Major', 'https://catalog.unc.edu/undergraduate/programs-study/music-major-ba/')
+    const medicAnthro = new major('Medical Anthropology Major','https://catalog.unc.edu/undergraduate/programs-study/medical-anthropology-major-ba/' )
+    const mediaJournalism = new major('Media and Journalism Major', 'https://catalog.unc.edu/undergraduate/programs-study/media-journalism-major-ba/')
+    const math = new major('Mathematics Major','https://catalog.unc.edu/undergraduate/programs-study/mathematics-major-bs/')
+    const manageSociety = new major('Management and Society Major','https://catalog.unc.edu/undergraduate/programs-study/management-society-major-ba/' )
+
+    const MajorList = [africanStudies, americanStudies, anthropology, archaeology, artHistory, asianStudies, biology, 
+        bioMedEngineering, biostats, business, chemistry, classics, clinicalLabSci, communication, compSci, 
+        contempEuroStudies, dataSci, dentalHygeine, drama, earthMarineSci, econ, english, enviHealthSci, enviSci, 
+        exerciseSportSci, geography, geologicSci, germSlavLang, globalStudies, healthPolicyMngmt, history, humDevFamSci,
+        infoSci, interdisciplinaryStudies, latinAmericanStudies, linguistics, manageSociety, math, mediaJournalism, 
+        medicAnthro, music, neuro, neurodiagnosticSleepSci, nursing, nutrition, peaceWarDefense, philosophy, phsyics, 
+        poliSci, psych, publicPolicy, radioSci, religiousStudies, romanceLang, sociology, stats, studioArt, womenGenderStudies
+    ]
+
+    return MajorList
+};
 
 function makeQuestion(string){
     const question = document.createElement("div");
@@ -18,6 +95,7 @@ function makeQuestion(string){
 
 function MakeAnswers(){
     const form = document.createElement("form");
+    form.setAttribute("id", "form")
 
     const answer1 = document.createElement("input");
     answer1.setAttribute("type", "radio");
