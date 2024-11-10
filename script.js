b460ec340867673dfd30c1caa669038ef3335eba
Ann Jins <annjins@ad.unc.edu>
2024-11-09 20:12:01 -0500

    h

--- script.js
+++ script.js
@@ -76,8 +76,8 @@ function Start() {
     AAADquestion.setAttribute("class", "question")
     const AAADstatement = document.createElement("h2")
     const AAADnode = document.createTextNode("How appealing does AAAD101: Introduction to Africa seem to you?");
-    AAADstatement.appendChild(node);
-    AAADquestion.appendChild(statement)
+    AAADstatement.appendChild(AAADnode);
+    AAADquestion.appendChild(AAADstatement)
 
     const AAADform = document.createElement("form");
     AADform.setAttribute("id", "form")
@@ -89,9 +89,9 @@ function Start() {
     const AAADlabel1 = document.createElement("label");
     AAADlabel1.setAttribute("for", "'first");
     const AAADnode1 = document.createTextNode("1");
-    AAADlabel1.appendChild(node1);
-    AAADform.appendChild(answer1);
-    AAADform.appendChild(label1);
+    AAADlabel1.appendChild(AAADnode1);
+    AAADform.appendChild(AAADanswer1);
+    AAADform.appendChild(AAADlabel1);
 
     const AAADanswer2 = document.createElement("input");
     AAADanswer2.setAttribute("type", "radio");
@@ -133,226 +133,226 @@ function Start() {
     AAADanswer5.setAttribute("type", "radio");
     AAADanswer5.setAttribute("id", "fifth");
     answer5.setAttribute("name", "group");
-    var label5 = document.createElement("label");
-    label5.setAttribute("for", "'fifth");
-    var node5 = document.createTextNode("5");
-    label5.appendChild(node5);
-    form.appendChild(answer5);
-    form.appendChild(label5);
+    const AAADlabel5 = document.createElement("label");
+    AAADlabel5.setAttribute("for", "'fifth");
+    const AAADnode5 = document.createTextNode("5");
+    AAADlabel5.appendChild(AAADnode5);
+    AAADform.appendChild(AAADanswer5);
+    AAADform.appendChild(AAADlabel5);
 
-    question.append(form)
+    AAADquestion.append(form)
 
-    element.replaceChild(question, remove)
+    element.replaceChild(AAADquestion, remove)
 
     //question 2
-    var question = document.createElement("div");
-    question.setAttribute("class", "question")
-    var statement = document.createElement("h2")
-    var node = document.createTextNode("How appealing does AMST101: This Place Called 'America' seem to you?");
-    statement.appendChild(node);
-    question.appendChild(statement)
+    const AMSTquestion = document.createElement("div");
+    AMSTquestion.setAttribute("class", "question")
+    const AMSTstatement = document.createElement("h2")
+    const AMSTnode = document.createTextNode("How appealing does AMST101: This Place Called 'America' seem to you?");
+    AMSTstatement.appendChild(AMSTnode);
+    AMSTquestion.appendChild(AMSTstatement)
 
-    var form = document.createElement("form");
-    form.setAttribute("id", "form")
+    const AMSTform = document.createElement("form");
+    AMSTform.setAttribute("id", "form")
 
-    var answer1 = document.createElement("input");
-    answer1.setAttribute("type", "radio");
-    answer1.setAttribute("id", "first");
-    answer1.setAttribute("name", "group");
-    var label1 = document.createElement("label");
-    label1.setAttribute("for", "'first");
-    var node1 = document.createTextNode("1");
-    label1.appendChild(node1);
-    form.appendChild(answer1);
-    form.appendChild(label1);
+    const AMSTanswer1 = document.createElement("input");
+    AMSTanswer1.setAttribute("type", "radio");
+    AMSTanswer1.setAttribute("id", "first");
+    AMSTanswer1.setAttribute("name", "group");
+    const AMSTlabel1 = document.createElement("label");
+    AMSTlabel1.setAttribute("for", "'first");
+    const AMSTnode1 = document.createTextNode("1");
+    AMSTlabel1.appendChild(AMSTnode1);
+    AMSTform.appendChild(AMSTanswer1);
+    AMSTform.appendChild(AMSTlabel1);
 
-    var answer2 = document.createElement("input");
-    answer2.setAttribute("type", "radio");
-    answer2.setAttribute("id", "second");
-    answer2.setAttribute("name", "group");
-    var label2 = document.createElement("label");
-    label2.setAttribute("for", "'second");
-    var node2 = document.createTextNode("2");
-    label2.appendChild(node2);
-    form.appendChild(answer2);
-    form.appendChild(label2);
+    const AMSTanswer2 = document.createElement("input");
+    AMSTanswer2.setAttribute("type", "radio");
+    AMSTanswer2.setAttribute("id", "second");
+    AMSTanswer2.setAttribute("name", "group");
+    const AMSTlabel2 = document.createElement("label");
+    AMSTlabel2.setAttribute("for", "'second");
+    const AMSTnode2 = document.createTextNode("2");
+    AMSTlabel2.appendChild(AMSTnode2);
+    AMSTform.appendChild(AMSTanswer2);
+    AMSTform.appendChild(AMSTlabel2);
 
-    var answer3 = document.createElement("input");
-    answer3.setAttribute("type", "radio");
-    answer3.setAttribute("id", "third");
-    answer3.setAttribute("name", "group");
-    var label3 = document.createElement("label");
-    label3.setAttribute("for", "'third");
-    var node3 = document.createTextNode("3");
-    label3.appendChild(node3);
-    form.appendChild(answer3);
-    form.appendChild(label3);
+    AMST AMSTanswer3 = document.createElement("input");
+    AMSTanswer3.setAttribute("type", "radio");
+    AMSTanswer3.setAttribute("id", "third");
+    AMSTanswer3.setAttribute("name", "group");
+    const AMSTlabel3 = document.createElement("label");
+    AMSTlabel3.setAttribute("for", "'third");
+    ocnst AMSTnode3 = document.createTextNode("3");
+    AMSTlabel3.appendChild(AMSTnode3);
+    AMSTform.appendChild(AMSTanswer3);
+    AMSTform.appendChild(AMSTlabel3);
 
-    var answer4 = document.createElement("input");
-    answer4.setAttribute("type", "radio");
-    answer4.setAttribute("id", "fourth");
-    answer4.setAttribute("name", "group");
-    var label4 = document.createElement("label");
-    label4.setAttribute("for", "'fourth");
-    var node4 = document.createTextNode("4");
-    label4.appendChild(node4);
-    form.appendChild(answer4);
-    form.appendChild(label4);
+    const AMSTanswer4 = document.createElement("input");
+    AMSTanswer4.setAttribute("type", "radio");
+    AMSTanswer4.setAttribute("id", "fourth");
+    AMSTanswer4.setAttribute("name", "group");
+    const AMSTlabel4 = document.createElement("label");
+    AMSTlabel4.setAttribute("for", "'fourth");
+    const AMSTnode4 = document.createTextNode("4");
+    AMSTlabel4.appendChild(AMSTnode4);
+    AMSTform.appendChild(AMSTanswer4);
+    AMSTform.appendChild(AMSTlabel4);
 
-    var answer5 = document.createElement("input");
-    answer5.setAttribute("type", "radio");
-    answer5.setAttribute("id", "fifth");
-    answer5.setAttribute("name", "group");
-    var label5 = document.createElement("label");
-    label5.setAttribute("for", "'fifth");
-    var node5 = document.createTextNode("5");
-    label5.appendChild(node5);
-    form.appendChild(answer5);
-    form.appendChild(label5);
+    const AMSTanswer5 = document.createElement("input");
+    AMSTanswer5.setAttribute("type", "radio");
+    AMSTanswer5.setAttribute("id", "fifth");
+    AMSTanswer5.setAttribute("name", "group");
+    const AMSTlabel5 = document.createElement("label");
+    AMSTlabel5.setAttribute("for", "'fifth");
+    const AMSTnode5 = document.createTextNode("5");
+    AMSTlabel5.appendChild(AMSTnode5);
+    AMSTform.appendChild(AMSTanswer5);
+    AMSTform.appendChild(AMSTlabel5);
 
-    question.append(form)
+    AMSTquestion.append(AMSTform)
     
-    element.appendChild(question)
+    element.appendChild(AMSTquestion)
 
     //question 3
-    var question = document.createElement("div");
-    question.setAttribute("class", "question")
-    var statement = document.createElement("h2")
-    var node = document.createTextNode("How appealing does ANTH148: Human Origins seem to you?");
-    statement.appendChild(node);
-    question.appendChild(statement)
+    const ANTHquestion = document.createElement("div");
+    ANTHquestion.setAttribute("class", "question")
+    const ANTHstatement = document.createElement("h2")
+    const ANTHnode = document.createTextNode("How appealing does ANTH148: Human Origins seem to you?");
+    const ANTHstatement.appendChild(ANTHnode);
+    ANTHquestion.appendChild(ANTHstatement)
 
-    var form = document.createElement("form");
-    form.setAttribute("id", "form")
+    const ANTHform = document.createElement("form");
+    ANTHform.setAttribute("id", "form")
 
-    var answer1 = document.createElement("input");
-    answer1.setAttribute("type", "radio");
-    answer1.setAttribute("id", "first");
-    answer1.setAttribute("name", "group");
-    var label1 = document.createElement("label");
-    label1.setAttribute("for", "'first");
-    var node1 = document.createTextNode("1");
-    label1.appendChild(node1);
-    form.appendChild(answer1);
-    form.appendChild(label1);
+    const ANTHanswer1 = document.createElement("input");
+    ANTHanswer1.setAttribute("type", "radio");
+    ANTHanswer1.setAttribute("id", "first");
+    ANTHanswer1.setAttribute("name", "group");
+    const ANTHlabel1 = document.createElement("label");
+    ANTHlabel1.setAttribute("for", "'first");
+    const ANTHnode1 = document.createTextNode("1");
+    ANTHlabel1.appendChild(ANTHnode1);
+    ANTHform.appendChild(ANTHanswer1);
+    ANTHform.appendChild(ANTHlabel1);
 
-    var answer2 = document.createElement("input");
-    answer2.setAttribute("type", "radio");
-    answer2.setAttribute("id", "second");
-    answer2.setAttribute("name", "group");
-    var label2 = document.createElement("label");
-    label2.setAttribute("for", "'second");
-    var node2 = document.createTextNode("2");
-    label2.appendChild(node2);
-    form.appendChild(answer2);
-    form.appendChild(label2);
+    const ANTHanswer2 = document.createElement("input");
+    ANTHanswer2.setAttribute("type", "radio");
+    ANTHanswer2.setAttribute("id", "second");
+    ANTHanswer2.setAttribute("name", "group");
+    const ANTHlabel2 = document.createElement("label");
+    ANTHlabel2.setAttribute("for", "'second");
+    cpnst ANTHnode2 = document.createTextNode("2");
+    ANTHlabel2.appendChild(ANTHnode2);
+    ANTHform.appendChild(ANTHanswer2);
+    ANTHform.appendChild(ANTHlabel2);
 
-    var answer3 = document.createElement("input");
-    answer3.setAttribute("type", "radio");
-    answer3.setAttribute("id", "third");
-    answer3.setAttribute("name", "group");
-    var label3 = document.createElement("label");
-    label3.setAttribute("for", "'third");
-    var node3 = document.createTextNode("3");
-    label3.appendChild(node3);
-    form.appendChild(answer3);
-    form.appendChild(label3);
+    const ANTHanswer3 = document.createElement("input");
+    ANTHanswer3.setAttribute("type", "radio");
+    ANTHanswer3.setAttribute("id", "third");
+    ANTHanswer3.setAttribute("name", "group");
+    ANTHvar label3 = document.createElement("label");
+    ANTHlabel3.setAttribute("for", "'third");
+    const ANTHnode3 = document.createTextNode("3");
+    ANTHlabel3.appendChild(ANTHnode3);
+    ANTHform.appendChild(ANTHanswer3);
+    ANTHform.appendChild(ANTHlabel3);
 
-    var answer4 = document.createElement("input");
-    answer4.setAttribute("type", "radio");
-    answer4.setAttribute("id", "fourth");
-    answer4.setAttribute("name", "group");
-    var label4 = document.createElement("label");
-    label4.setAttribute("for", "'fourth");
-    var node4 = document.createTextNode("4");
-    label4.appendChild(node4);
-    form.appendChild(answer4);
-    form.appendChild(label4);
+    const ANTHanswer4 = document.createElement("input");
+    ANTHanswer4.setAttribute("type", "radio");
+    ANTHanswer4.setAttribute("id", "fourth");
+    ANTHanswer4.setAttribute("name", "group");
+    const ANTHlabel4 = document.createElement("label");
+    ANTHlabel4.setAttribute("for", "'fourth");
+    const ANTHnode4 = document.createTextNode("4");
+    ANTHlabel4.appendChild(ANTHnode4);
+    ANTHform.appendChild(ANTHanswer4);
+    ANTHform.appendChild(ANTHlabel4);
 
-    var answer5 = document.createElement("input");
-    answer5.setAttribute("type", "radio");
-    answer5.setAttribute("id", "fifth");
-    answer5.setAttribute("name", "group");
-    var label5 = document.createElement("label");
-    label5.setAttribute("for", "'fifth");
-    var node5 = document.createTextNode("5");
-    label5.appendChild(node5);
-    form.appendChild(answer5);
-    form.appendChild(label5);
+    const ANTHanswer5 = document.createElement("input");
+    ANTHanswer5.setAttribute("type", "radio");
+    ANTHanswer5.setAttribute("id", "fifth");
+    ANTHanswer5.setAttribute("name", "group");
+    const ANTHlabel5 = document.createElement("label");
+    ANTHlabel5.setAttribute("for", "'fifth");
+    const ANTHnode5 = document.createTextNode("5");
+    ANTHlabel5.appendChild(ANTHnode5);
+    ANTHform.appendChild(ANTHanswer5);
+    ANTHform.appendChild(ANTHlabel5);
 
-    question.append(form)
+    ANTHquestion.append(ANTHform)
     
-    element.appendChild(question)
+    element.appendChild(ANTHquestion)
 
     //question 4
-    var question = document.createElement("div");
-    question.setAttribute("class", "question")
-    var statement = document.createElement("h2")
-    var node = document.createTextNode("How appealing does APPL101: Exploring Engineering seem to you?");
-    statement.appendChild(node);
-    question.appendChild(statement)
+    const AAPLquestion = document.createElement("div");
+    AAPLquestion.setAttribute("class", "question")
+    const AAPLstatement = document.createElement("h2")
+    const AAPLnode = document.createTextNode("How appealing does APPL101: Exploring Engineering seem to you?");
+    AAPLstatement.appendChild(AAPLnode);
+    AAPLquestion.appendChild(AAPLstatement)
 
-    var form = document.createElement("form");
-    form.setAttribute("id", "form")
+    const AAPLform = document.createElement("form");
+    AAPLform.setAttribute("id", "form")
 
-    var answer1 = document.createElement("input");
-    answer1.setAttribute("type", "radio");
-    answer1.setAttribute("id", "first");
-    answer1.setAttribute("name", "group");
-    var label1 = document.createElement("label");
-    label1.setAttribute("for", "'first");
-    var node1 = document.createTextNode("1");
-    label1.appendChild(node1);
-    form.appendChild(answer1);
-    form.appendChild(label1);
+    const AAPLanswer1 = document.createElement("input");
+    AAPLanswer1.setAttribute("type", "radio");
+    AAPLanswer1.setAttribute("id", "first");
+    AAPLanswer1.setAttribute("name", "group");
+    const AAPLlabel1 = document.createElement("label");
+    AAPLlabel1.setAttribute("for", "'first");
+    const AAPLnode1 = document.createTextNode("1");
+    AAPLlabel1.appendChild(AAPLnode1);
+    AAPLform.appendChild(AAPLanswer1);
+    AAPLform.appendChild(AAPLlabel1);
 
-    var answer2 = document.createElement("input");
-    answer2.setAttribute("type", "radio");
-    answer2.setAttribute("id", "second");
-    answer2.setAttribute("name", "group");
-    var label2 = document.createElement("label");
-    label2.setAttribute("for", "'second");
-    var node2 = document.createTextNode("2");
-    label2.appendChild(node2);
-    form.appendChild(answer2);
-    form.appendChild(label2);
+    const AAPLanswer2 = document.createElement("input");
+    AAPLanswer2.setAttribute("type", "radio");
+    AAPLanswer2.setAttribute("id", "second");
+    AAPLanswer2.setAttribute("name", "group");
+    const AAPLlabel2 = document.createElement("label");
+    AAPLlabel2.setAttribute("for", "'second");
+    const AAPLnode2 = document.createTextNode("2");
+    AAPLlabel2.appendChild(AAPLnode2);
+    AAPLform.appendChild(AAPLanswer2);
+    AAPLform.appendChild(AAPLlabel2);
 
-    var answer3 = document.createElement("input");
-    answer3.setAttribute("type", "radio");
-    answer3.setAttribute("id", "third");
-    answer3.setAttribute("name", "group");
-    var label3 = document.createElement("label");
-    label3.setAttribute("for", "'third");
-    var node3 = document.createTextNode("3");
-    label3.appendChild(node3);
-    form.appendChild(answer3);
-    form.appendChild(label3);
+    const AAPLanswer3 = document.createElement("input");
+    AAPLanswer3.setAttribute("type", "radio");
+    AAPLanswer3.setAttribute("id", "third");
+    AAPLanswer3.setAttribute("name", "group");
+    const AAPLlabel3 = document.createElement("label");
+    AAPLlabel3.setAttribute("for", "'third");
+    const AAPLnode3 = document.createTextNode("3");
+    AAPLlabel3.appendChild(AAPLnode3);
+    AAPLform.appendChild(AAPLanswer3);
+    AAPLform.appendChild(AAPLlabel3);
 
-    var answer4 = document.createElement("input");
-    answer4.setAttribute("type", "radio");
-    answer4.setAttribute("id", "fourth");
-    answer4.setAttribute("name", "group");
-    var label4 = document.createElement("label");
-    label4.setAttribute("for", "'fourth");
-    var node4 = document.createTextNode("4");
-    label4.appendChild(node4);
-    form.appendChild(answer4);
-    form.appendChild(label4);
+    const AAPLanswer4 = document.createElement("input");
+    AAPLanswer4.setAttribute("type", "radio");
+    AAPLanswer4.setAttribute("id", "fourth");
+    AAPLanswer4.setAttribute("name", "group");
+    const AAPLlabel4 = document.createElement("label");
+    AAPLlabel4.setAttribute("for", "'fourth");
+    const AAPLnode4 = document.createTextNode("4");
+    AAPLlabel4.appendChild(AAPLnode4);
+    AAPLform.appendChild(AAPLanswer4);
+    AAPLform.appendChild(AAPLlabel4);
 
-    var answer5 = document.createElement("input");
-    answer5.setAttribute("type", "radio");
-    answer5.setAttribute("id", "fifth");
-    answer5.setAttribute("name", "group");
-    var label5 = document.createElement("label");
-    label5.setAttribute("for", "'fifth");
-    var node5 = document.createTextNode("5");
-    label5.appendChild(node5);
-    form.appendChild(answer5);
-    form.appendChild(label5);
+    const AAPLanswer5 = document.createElement("input");
+    AAPLanswer5.setAttribute("type", "radio");
+    AAPLanswer5.setAttribute("id", "fifth");
+    AAPLanswer5.setAttribute("name", "group");
+    const AAPLlabel5 = document.createElement("label");
+    AAPLlabel5.setAttribute("for", "'fifth");
+    const AAPLnode5 = document.createTextNode("5");
+    AAPLlabel5.appendChild(AAPLnode5);
+    AAPLform.appendChild(AAPLanswer5);
+    AAPLform.appendChild(label5);
 
-    question.append(form)
+    AAPLquestion.append(AAPLform)
     
-    element.appendChild(question)
+    element.appendChild(AAPLquestion)
 
     //question 5
     var question = document.createElement("div");
